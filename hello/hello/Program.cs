d322ae5f62c4bc0d6751aa4cb054b066b1e60d16
Positron234 <andreigaivoronskikh120@gmail.com>
2023-04-21 18:07:24 +0700

    add message

---
+++ hello/hello/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Oh, hi, Mark!");
+            Console.ReadKey();
+        }
+    }
+}
+
